5eb812b5c56ad76f4f5c8fd0f104a38fb4ef8a6d
Li Zhe <software@bjtu.edu.cn>
2018-03-15 16:06:24 +0800

    0315

--- logic/userlogin.php
+++ logic/userlogin.php
@@ -2,6 +2,57 @@
 if ($_POST['userloginname'] == "" || $_POST['userlogincath'] == ""){
 	echo "没有传入参数你是傻逼吗？";
 }else{
-	echo "进行判断";
+	//Input
+	$userlogin = $_POST['userloginname'];
+	$usercath = $_POST['userlogincath'];
+	
+	//Output
+	$loginstate = ""; //state: 200 success 301 wrong username/password 400 internal error
+	$userid = "";
+	$usertoken = "";
+	$usertokenexpire = "";
+	
+	//Link DB
+	$con = mysql_connect("localhost","tc","lizhe20080722");
+    if (!$con)
+       {
+        die('Could not connect: ' . mysql_error());
+       }
+	mysql_select_db("tc");
+	$result = mysql_query("SELECT * FROM tc_user WHERE tc_user_loginname = '".$userlogin."'");
+	$row=mysql_fetch_array($result);
+		
+	if $row['tc_user_id'] == "" {
+		$loginstate = "301";
+	} else {
+		if ($row['tc_user_password'] == $usercath) {
+			$str = md5(uniqid(md5(microtime(true).$row['tc_user_id']),true));
+			$token = sha1($str);
+			$expiretime = strtotime("+1 day");
+			$insert = "INSERT INTO tc_usertoken (tc_usertoken_uid,tc_usertoken_token,tc_usertoken_timelimit) 
+			           VALUES (".$row['tc_user_id'].",'".$usertoken."',".$expiretime.")";
+			if (mysql_query($insert)) != false {
+				$loginstate = "200";
+				$userid = $row['tc_user_id'];
+				$usertoken = $token;
+				$usertokenexpire = $expiretime;
+			} else {
+				$loginstate = "200"
+		    }
+		} else {
+			$loginstate = "301";
+		}
+	}
+	$output = array (
+	             'loginState' => $loginstate,
+	             'userID' => $userid,
+                 'userToken' => $usertoken,
+				 'userTokenExpire' => $usertokenexpire);
+    echo json_encode($output);
+	
+	
+	
+	
+	
 }
 ?>
