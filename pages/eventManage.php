7e53b2415e761f9e99a465178dd30de044e6680e
Li Zhe <software@bjtu.edu.cn>
2018-03-18 03:56:46 +0800

    0318

--- pages/eventManage.php
+++ pages/eventManage.php
@@ -163,7 +163,7 @@
         var date = new Date(timestamp * 1000);
         Y = date.getFullYear() + '-';
         M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-        D = date.getDate() + ' ';
+        D = date.getDate() + '';
         h = date.getHours() + ':';
         m = date.getMinutes() + ':';
         s = date.getSeconds();
