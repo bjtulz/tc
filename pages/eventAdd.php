f95af31995d3e1693f7d546033b9bf6f16ff83ff
Li Zhe <software@bjtu.edu.cn>
2018-03-18 00:50:56 +0800

    0317

--- pages/eventAdd.php
+++ pages/eventAdd.php
@@ -153,9 +153,9 @@
 				},
 				function(data){
 					switch (data.state){
-						case 200:
+						
 						    $('#inputarea').append("<div class='alert alert-success alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Succeed to add new event. Event ID:"+data.newEventID+" Event name:"+data.newEventName+"("+timestampToTime(data.newEventStart)+"~"+timestampToTime(data.newEventEnd)+") Ticket number limit:"+data.newEventTicketLimit+"</div>");
-						default:
+						
                             $('#inputarea').append("<div class='alert alert-danger alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Failed to add new event</div>");
 					}
 				  
