be3d6a2b64c4137f45974699fdbbc9723692d4a0
Li Zhe <software@bjtu.edu.cn>
2018-03-17 22:42:10 +0800

    0317

--- logic/addEvent.php
+++ logic/addEvent.php
@@ -42,8 +42,7 @@ if ($_POST['userID'] == "" ||
 								   "tc_usertoken_uid" => $userID,
 								   "tc_usertoken_token" => $userToken
 								   ]]);
-		echo $tokenData;
-		if ($tokenData == "" ) {
+		if (count($tokenData) == 0 ) {
 			$userStatus = 301; //token does not exist
 		} else if ($tokenData[0]["tc_usertoken_timelimit"] <= $current ){
 			$userStatus = 302; //token expired; 
