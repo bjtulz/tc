43141a7266c71ff53d88510ba5ce024c591f0bd6
Li Zhe <software@bjtu.edu.cn>
2018-03-18 23:16:11 +0800

    0318

--- pages/ticketByTag.php
+++ pages/ticketByTag.php
@@ -141,7 +141,7 @@
 			var userid = 1;
 			var usertoken = "1a39cfe7ea929a253c41d215fb46668659ddf8f0";
 			
-			$.post("../logic/getTicketsbytag.php",
+			$.post("../logic/getTicketbytag.php",
 				{
 				  userID:userid,
 				  userToken:usertoken,
